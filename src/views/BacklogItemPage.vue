842724d5324fdb0d4aa16e4a207e8a2cde120bf7
Eddie <t111598093@ntut.org.tw>
2024-06-06 00:45:06 +0800

    [Bug Fixing]Fixed Create Task bug while estimated hours is null

--- src/views/BacklogItemPage.vue
+++ src/views/BacklogItemPage.vue
@@ -160,13 +160,13 @@ import { AssignStoryPoint, CreateTask } from '../api/projectApi';
         newTask: {
           title: null,
           priority: null,
-          hours: 0,
+          hours: null,
           description: null
         },
         defaultTask: {
           title: null,
           priority: null,
-          hours: 0,
+          hours: null,
           description: null
         },
         headers: [
@@ -217,8 +217,8 @@ import { AssignStoryPoint, CreateTask } from '../api/projectApi';
         this.close();
       },
       async createTask() {
-        if (this.newTask.hours == null) {
-          this.newTask.hours = 0;
+        if (this.newTask.hours == null){
+          this.newTask.hours = 0
         }
         var response = await CreateTask(
           this.backlogItem.id,
